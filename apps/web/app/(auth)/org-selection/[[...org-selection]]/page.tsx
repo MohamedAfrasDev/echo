c40321eaeb575e5fe6134bed903a066860b58bfc
MohamedAfrasDev <mohamedafrasai@gmail.com>
2026-02-24 06:39:30 +0530

    04: Organization

---
+++ apps/web/app/(auth)/org-selection/[[...org-selection]]/page.tsx
@@ -0,0 +1,8 @@
+import { OrgSelectionView } from '@/modules/auth/ui/views/org-select-view'
+import React from 'react'
+
+const page = () => {
+    return <OrgSelectionView />
+}
+
+export default page
